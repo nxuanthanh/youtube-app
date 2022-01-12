458daed35758f5bddb924965489dc662957d1c07
n.xuanthanh <n.xuanthanhh@gmail.com>
2022-01-12 10:10:55 +0700

    event sidebar

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,9 +1,29 @@
-"use strict";
+'use strict';
 
-const menuBtn = document.querySelector(".header__navbar--menu");
-const sidebar = document.querySelector(".sidebar");
-if (menuBtn) {
-  menuBtn.addEventListener("click", () => {
-    sidebar.classList.toggle("show");
+const menuBtn = document.querySelector('.header__navbar--menu');
+const sidebar = document.querySelector('.sidebar');
+const sidebarListSmall = document.querySelector('.sidebar__list-sm');
+const sidebarList = document.querySelector('.sidebar__list');
+
+function attachEventsSidebar() {
+  if (!menuBtn || !sidebar) return;
+
+  sidebarListSmall.style.display = 'none';
+
+  menuBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('show');
+    const isShow = sidebar.classList.contains('show');
+
+    if (isShow) {
+      sidebarList.style.display = 'block';
+      sidebarListSmall.style.display = 'none';
+    } else {
+      sidebarList.style.display = 'none';
+      sidebarListSmall.style.display = 'block';
+    }
   });
 }
+
+(() => {
+  attachEventsSidebar();
+})();
